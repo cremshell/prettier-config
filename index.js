719b123b3c3e289667cc39540f41957e167d0263
cremshell <cremshel@gmail.com>
2025-04-06 02:24:50 +0900

    export config

---
+++ index.js
@@ -0,0 +1,21 @@
+const config = {
+  printWidth: 80,
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  objectWrap: 'collapse',
+  bracketSameLine: false,
+  arrowParens: 'always',
+  insertPragma: false,
+  htmlWhitespaceSensitivity: 'css',
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+  singleAttributePerLine: false,
+};
+
+export default config;
